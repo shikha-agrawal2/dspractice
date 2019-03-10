e8c68519cb5bb34abbb280b2e895ac9df7c2f848
Shikha <shikha.agrawal79@gmail.com>
2019-03-11 00:15:41 +0530

    test

---
+++ src/ReadMeGitPushThroughIntellij.java
@@ -0,0 +1,8 @@
+public class ReadMeGitPushThroughIntellij {
+
+//    1. In IntelliJ IDEA menu: VCS -> Import into Version Control -> Create Git Repository
+//    2. In pop up window select your project and click ok.
+//    3. Commit all necessary files to you local branch master.
+//    4. Right click on project: go to GIT -> Repository -> Push (this is to push committed changes to Git)
+
+}
